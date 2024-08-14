6f9669633f67ff48f8ff1ca099f3825d005e4048
Kavalorn <kavalorn12345@gmail.com>
2024-08-14 12:31:01 +0300

    fix/cleanup from previos experiments

--- src/entities/Peer/store/store.ts
+++ src/entities/Peer/store/store.ts
@@ -11,26 +11,7 @@ export interface PeerStore {
 
 export const usePeerStore = create<PeerStore>()(immer((set) => {
         const room = joinRoom(config, 'room1')
-
-        room.onPeerJoin(peerId => {
-            console.log(`${peerId} joined`);
-            // Assuming trystero provides a way to access the peer connection
-            const peerConnection = room.getPeers()[peerId];
-            console.log('tada: ', peerConnection);
-            if (peerConnection) {
-                peerConnection.onconnectionstatechange = (event) => {
-                    try {
-                        const pc = event.target as RTCPeerConnection;
-                        if (pc.connectionState === 'failed') {
-                            console.error('Connection failed.');
-                        }
-                    } catch (error) {
-                        console.error('Connection state change error:', error);
-                    }
-                };
-            }
-        });
-    
+        room.onPeerJoin(peerId => console.log(`${peerId} joined`));
         room.onPeerLeave(peerId => console.log(`${peerId} left`));
     return ({
         room: room,
