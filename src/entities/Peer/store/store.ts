213e3f7bf5d66222db61067c0630ab1ad89becff
Kavalorn <kavalorn12345@gmail.com>
2024-08-14 12:28:39 +0300

    fix/changed trystero strategy to torrent because of networking issues with default (nostr)

--- src/entities/Peer/store/store.ts
+++ src/entities/Peer/store/store.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
-import {joinRoom, Room, selfId} from 'trystero';
+import {joinRoom, Room, selfId} from 'trystero/torrent';
 
 const config = {appId: 'tadaa'}
 
@@ -10,10 +10,28 @@ export interface PeerStore {
 }
 
 export const usePeerStore = create<PeerStore>()(immer((set) => {
-    const room = joinRoom(config, 'room1')
-    room.onPeerJoin(peerId => console.log(`${peerId} joined`))
-    room.onPeerLeave(peerId => console.log(`${peerId} left`))
+        const room = joinRoom(config, 'room1')
 
+        room.onPeerJoin(peerId => {
+            console.log(`${peerId} joined`);
+            // Assuming trystero provides a way to access the peer connection
+            const peerConnection = room.getPeers()[peerId];
+            console.log('tada: ', peerConnection);
+            if (peerConnection) {
+                peerConnection.onconnectionstatechange = (event) => {
+                    try {
+                        const pc = event.target as RTCPeerConnection;
+                        if (pc.connectionState === 'failed') {
+                            console.error('Connection failed.');
+                        }
+                    } catch (error) {
+                        console.error('Connection state change error:', error);
+                    }
+                };
+            }
+        });
+    
+        room.onPeerLeave(peerId => console.log(`${peerId} left`));
     return ({
         room: room,
         selfId: selfId
